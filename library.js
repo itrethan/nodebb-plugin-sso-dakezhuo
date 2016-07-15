34ad72372fbc847771053863392eaa20b2367a2c
itrethan <ethan.wuqiong@gmail.com>
2016-07-14 22:50:05 -0700

    fxied

--- library.js
+++ library.js
@@ -6,7 +6,7 @@
 	Dakezhuo.getStrategy = function(strategies, callback) {
 		strategies.push({
 			name: 'dakezhuo',
-			url: 'http://www.dakezhuo.com/landing/loginXunfei?redirect=bbs',
+			url: 'http://www.dakezhuo.com/landing/loginBbsXunfei',
 			callbackURL: '/auth/dakezhuo/callback',
 			icon: 'fa-sign-in',
 		});
